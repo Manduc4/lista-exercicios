0204296e6a95927d0d7f3106ac961fe8ea9a1c8f
Caio Manduca <caiomanducas@gmail.com>
2023-03-20 21:53:05 -0400

    Exercício 1 Finalizado.

---
+++ README.md
@@ -0,0 +1,26 @@
+# 1) A empresa TECNOLOGIA LTDA deseja informatizar sua folha de pagamento, desenvolva
+# um algoritmo que faca esse controle, para isso:
+
+  # a) Colete as seguintes informações:
+  # Nome;
+  # Quantidade de horas trabalhadas no mês;
+  # Turno de trabalho (M = Matutino, V = Vespertino e N = Noturno)
+  # Categoria (G = Gerente e O = Operário)
+
+  # b) Salve as informações dos funcionários em uma lista, não permitindo que sejam
+  # informados turnos e categorias inexistentes e não aceitar valores vazios e/ou nulos.
+
+  # c) Calcule o valor da hora trabalhada conforme a tabela a seguir.
+  # (Adote o valor de R$ 1.320,00 para o salário mínimo.)
+  # Categoria Turno Valor da hora trabalhada
+
+  # G N 10% do salário mínimo
+  # G M ou V 15% do salário mínimo
+  # O N 9% do salário mínimo
+  # O M ou V 14% do salário mínimo
+
+  # d) Calcule e mostre o salário de cada funcionário com base nas horas trabalhadas e
+  # no valor calculado para hora trabalhada de cada funcionário.
+
+  # ============================================================================================================
+
--- exercicio-01.py
+++ exercicio-01.py
@@ -1,56 +1,113 @@
-# 1) A empresa TECNOLOGIA LTDA deseja informatizar sua folha de pagamento, desenvolva
-# um algoritmo que faça esse controle, para isso:
-# a) Colete as seguintes informações:
-# Nome;
-# Quantidade de horas trabalhadas no mês;
-# Turno de trabalho (M = Matutino, V = Vespertino e N = Noturno)
-# Categoria (G = Gerente e O = Operário)
-# b) Salve as informações dos funcionários em uma lista, não permitindo que sejam
-# informados turnos e categorias inexistentes e não aceitar valores vazios e/ou nulos.
-# c) Calcule o valor da hora trabalhada conforme a tabela a seguir.
-# (Adote o valor de R$ 1.320,00 para o salário mínimo.)
-# Categoria Turno Valor da hora trabalhada
 
-# G N 10% do salário mínimo
-# G M ou V 15% do salário mínimo
-# O N 9% do salário mínimo
-# O M ou V 14% do salário mínimo
+# variaveis globais
+salario_base = 1320.00
 
-# d) Calcule e mostre o salário de cada funcionário com base nas horas trabalhadas e
-# no valor calculado para hora trabalhada de cada funcionário.
+# cargos
+gerente = 'G'
+operario = 'O'
 
-# ============================================================================================================
+# turnos
+matutino = 'M'
+vespertino = 'V'
+noturno = 'N'
 
-# nome = input(str('Nome: '))
-# turno = input(str('Turno (M-Matutino, V-Vespertino ou N-Noturno): '))
-# categoria = input(str('Categoria: (G-Gerente ou O-Operário): '))
+# erros
+errors = []
+nome_vazio = 'O Nome e Obrigatorio.'
+turno_vazio = 'O Turno e Obrigatorio.'
+cargo_vazio = 'O Cargo e Obrigatorio.'
 
-salario_minimo = 1320
-valor_hora_trabalhada = 0
+turno_incorreto = 'Insira o turno de acordo com a legenda (M-Matutino, V-Vespertino ou N-Noturno): '
+cargo_incorreto = 'Insira o cargo de acordo com a legenda (G-Gerente ou O-Operario): '
 
-nome = 'Caio'
-turno = 'N'
-categoria = 'G'
-qtd_horas_trabalhadas = 2
+def inputNome():
+    while (True):
+        nome = input('Nome: ').upper()
+        if (len(nome) == 0):
+            print(nome_vazio)
+        else:
+          break
+    return nome
 
-lista = [nome, turno, categoria]
+def inputTurno():
+    while (True):
+        turno = input('Turno: ').upper()
+        if (len(turno) == 0):
+            print(turno_vazio)
+        elif (turno != 'M') and (turno != 'V') and (turno != 'N'):
+            print(turno_incorreto)
+        else:
+            break
+    return turno
 
-if categoria == 'G':
-  if turno == 'M' or turno == 'V':
-    valor_hora_trabalhada = salario_minimo * 0.15
+def inputCargo():
+    while (True):
+        cargo = input('Cargo: ').upper()
+        if (len(cargo) == 0):
+            print(cargo_vazio)
+        elif (cargo != 'G') and (cargo != 'O'):
+            print(cargo_incorreto)
+        else:
+            break
+    return cargo
+
+def inputQuantidadeHorasTrabalhadas():
+    while (True):
+        qtd_horas_trabalhadas = int(input('Quantidade de Horas Trabalhadas: '))
+        if (len(str(qtd_horas_trabalhadas)) == 0):
+            print('A quantidade de horas é Obrigatória')
+        else:
+            break
+    return qtd_horas_trabalhadas
+        
+def generalInput():
+    nome = inputNome()
+    turno = inputTurno()
+    cargo = inputCargo()
+    qtd_horas_trabalhadas = inputQuantidadeHorasTrabalhadas()
+    return [nome, turno, cargo, qtd_horas_trabalhadas]
+
+
+def getValorPercentual(turno, cargo):
+    valor_percentual = 0
+    if cargo == gerente:
+        # Categoria de Gerente
+        if turno == matutino or turno == vespertino:
+            # turno matutino ou vespertino
+            valor_percentual = 0.15
+        else:
+            # turno noturno
+            valor_percentual = 0.10
+    if cargo == operario:
+        # Categoria de Operario
+        if turno == matutino or turno == vespertino:
+            # turno matutino ou vespertino
+            valor_percentual = 0.14
+        else:
+            # turno noturno
+            valor_percentual = 0.09
+    return valor_percentual
+
+
+def getSalarioFinal(valor_percentual, qtd_horas_trabalhadas):
+    valor_hora_trabalhada = salario_base * valor_percentual
     salario_final = valor_hora_trabalhada * qtd_horas_trabalhadas
-  else:
-    valor_hora_trabalhada = salario_minimo * 0.10
-    salario_final = valor_hora_trabalhada * qtd_horas_trabalhadas
-    
-if categoria == 'O':
-  if turno == 'M' or turno == 'V':
-    valor_hora_trabalhada = salario_minimo * 0.14
-    salario_final = valor_hora_trabalhada * qtd_horas_trabalhadas
-  else:
-    valor_hora_trabalhada = salario_minimo * 0.09
-    salario_final = valor_hora_trabalhada * qtd_horas_trabalhadas
+    return salario_final
 
-print('Salário Final do Funcionário: R$', salario_final)
-    
 
+def formatToReal(salario):
+    return 'R$' + str(salario).replace('.', ',')
+
+
+def root():
+    dados_de_entrada = generalInput()
+    print(dados_de_entrada)
+    valor_percentual = getValorPercentual(dados_de_entrada[1], dados_de_entrada[2])
+    salario_final = getSalarioFinal(valor_percentual, dados_de_entrada[3])
+    output = 'Salario final: ' + formatToReal(salario_final)
+    print(output)
+
+
+# root code
+root()
+# end root code
